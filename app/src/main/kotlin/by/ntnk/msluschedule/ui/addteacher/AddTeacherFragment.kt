2ad3578b58b83dbf2101cb13cf7b60490e42fbb3
Kirill Antonik <antkirp@gmail.com>
2018-01-27 22:09:40 +0300

    Initial AddTeacher dialog layout

--- app/src/main/kotlin/by/ntnk/msluschedule/ui/addteacher/AddTeacherFragment.kt
+++ app/src/main/kotlin/by/ntnk/msluschedule/ui/addteacher/AddTeacherFragment.kt
@@ -1,6 +1,11 @@
 package by.ntnk.msluschedule.ui.addteacher
 
+import android.app.Dialog
 import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.View
+import by.ntnk.msluschedule.R
 import by.ntnk.msluschedule.mvp.views.MvpDialogFragment
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
@@ -12,6 +17,24 @@ class AddTeacherFragment : MvpDialogFragment<AddTeacherPresenter, AddTeacherView
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val layout = View.inflate(activity, R.layout.add_teacher_view, null)
+        return initMaterialDialog(layout)
+    }
+
+    private fun initMaterialDialog(layout: View): Dialog {
+        return AlertDialog.Builder(activity!!)
+                .setTitle((R.string.add_teacher_title))
+                .setView(layout)
+                .setPositiveButton(R.string.button_add) { _, _ ->
+                    dismiss()
+                }
+                .setNegativeButton(R.string.button_cancel) { _, _ ->
+                    dismiss()
+                }
+                .create()
     }
 
     override fun onCreatePresenter(): AddTeacherPresenter {
