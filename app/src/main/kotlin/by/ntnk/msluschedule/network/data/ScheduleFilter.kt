714b74619502fd3d90a0f815236780770a90c3b0
Kirill Antonik <antkirp@gmail.com>
2018-02-02 22:37:24 +0300

    ScheduleFilter class

---
+++ app/src/main/kotlin/by/ntnk/msluschedule/network/data/ScheduleFilter.kt
@@ -0,0 +1,17 @@
+package by.ntnk.msluschedule.network.data
+
+import java.util.AbstractMap.SimpleEntry
+
+data class ScheduleFilter(val data: LinkedHashMap<Int, String>) {
+    val size
+        get() = data.size
+
+    fun getEntry(key: Int): Map.Entry<Int, String> {
+        val value = data.getValue(key)
+        return SimpleEntry(key, value)
+    }
+
+    fun valueAt(position: Int): String = data.values.toList()[position]
+
+    fun keyAt(position: Int): Int = data.keys.toList()[position]
+}
