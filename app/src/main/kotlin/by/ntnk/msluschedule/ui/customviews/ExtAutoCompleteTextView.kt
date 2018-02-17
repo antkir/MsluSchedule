623826bba26a486a3c1620f1662ff57c3242b42c
Kirill Antonik <antkirp@gmail.com>
2018-02-18 00:40:53 +0300

    ui: Extend AutoCompleteTextView

---
+++ app/src/main/kotlin/by/ntnk/msluschedule/ui/customviews/ExtAutoCompleteTextView.kt
@@ -0,0 +1,28 @@
+package by.ntnk.msluschedule.ui.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.AutoCompleteTextView
+
+class ExtAutoCompleteTextView : AutoCompleteTextView {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr)
+
+    override fun onKeyPreIme(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && isPopupShowing) {
+            val inputManager =
+                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            if (inputManager.hideSoftInputFromWindow(findFocus().windowToken,
+                            InputMethodManager.HIDE_NOT_ALWAYS)) {
+                return true
+            }
+        }
+        return super.onKeyPreIme(keyCode, event)
+    }
+}
