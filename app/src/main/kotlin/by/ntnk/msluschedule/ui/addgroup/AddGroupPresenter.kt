4b568b95373d9c917b1efb6d17aa1a8c75434e05
Kirill Antonik <antkirp@gmail.com>
2018-02-17 22:56:44 +0300

    ui: addgroup: Basic Presenter

--- app/src/main/kotlin/by/ntnk/msluschedule/ui/addgroup/AddGroupPresenter.kt
+++ app/src/main/kotlin/by/ntnk/msluschedule/ui/addgroup/AddGroupPresenter.kt
@@ -1,6 +1,32 @@
 package by.ntnk.msluschedule.ui.addgroup
 
 import by.ntnk.msluschedule.mvp.Presenter
+import by.ntnk.msluschedule.network.NetworkRepository
+import by.ntnk.msluschedule.utils.COURSE_VALUE
+import io.reactivex.Scheduler
 import javax.inject.Inject
 
-class AddGroupPresenter @Inject constructor() : Presenter<AddGroupView>()
+class AddGroupPresenter
+@Inject constructor(private val networkRepository: NetworkRepository) : Presenter<AddGroupView>() {
+    private val course: Int = COURSE_VALUE
+    var faculty: Int = 0
+        private set
+
+    fun getFacultyScheduleFilter(uiScheduler: Scheduler) {
+        networkRepository.getFaculties()
+                .observeOn(uiScheduler)
+                .subscribe(
+                        { view!!.populateFacultiesView(it) },
+                        { it.printStackTrace() }
+                )
+    }
+
+    fun getScheduleGroups(uiScheduler: Scheduler) {
+        networkRepository.getGroups(faculty, course)
+                .observeOn(uiScheduler)
+                .subscribe(
+                        { view!!.populateFacultiesView(it) },
+                        { it.printStackTrace() }
+                )
+    }
+}
--- app/src/main/kotlin/by/ntnk/msluschedule/utils/Utils.kt
+++ app/src/main/kotlin/by/ntnk/msluschedule/utils/Utils.kt
@@ -1,3 +1,7 @@
 package by.ntnk.msluschedule.utils
 
 const val EMPTY_STRING = ""
+
+// Passing year = 0 and any valid course value to the server
+// allows to get all groups of the selected faculty
+const val COURSE_VALUE = 1
