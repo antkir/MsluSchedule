a23d24f19325ea1b00fe39d95ce90a6001c6054a
Kirill Antonik <antkirp@gmail.com>
2018-03-14 13:34:50 +0300

    db: Initial db data mapper

---
+++ app/src/main/kotlin/by/ntnk/msluschedule/db/DatabaseDataMapper.kt
@@ -0,0 +1,31 @@
+package by.ntnk.msluschedule.db
+
+import by.ntnk.msluschedule.data.StudyGroup
+import by.ntnk.msluschedule.data.Teacher
+import by.ntnk.msluschedule.db.data.ScheduleContainer
+import by.ntnk.msluschedule.db.data.Week
+import by.ntnk.msluschedule.di.PerApp
+import by.ntnk.msluschedule.network.data.ScheduleFilter
+import by.ntnk.msluschedule.utils.ScheduleType
+import java.util.ArrayList
+import javax.inject.Inject
+
+@PerApp
+class DatabaseDataMapper @Inject constructor() {
+    fun map(data: ScheduleFilter, containerId: Int): List<Week> {
+        val weeks = ArrayList<Week>()
+        for (i in 0 until data.size) {
+            val key = data.keyAt(i)
+            val value = data.valueAt(i)
+            val week = Week(containerId, key, value)
+            weeks.add(week)
+        }
+        return weeks
+    }
+
+    fun map(data: StudyGroup): ScheduleContainer =
+            ScheduleContainer(data.id, data.name, ScheduleType.STUDYGROUP, data.faculty, data.course, data.year)
+
+    fun map(data: Teacher): ScheduleContainer =
+            ScheduleContainer(data.id, data.name, ScheduleType.TEACHER, 0, 0, data.year)
+}
