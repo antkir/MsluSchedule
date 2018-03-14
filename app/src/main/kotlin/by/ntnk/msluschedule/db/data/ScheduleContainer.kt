bc0ac57c58baa35c001706075c370eabe067811f
Kirill Antonik <antkirp@gmail.com>
2018-03-14 13:31:08 +0300

    db: data: move ScheduleContainer's type from string to enum
    * Add Room TypeConverter for ScheduleType

--- app/src/main/kotlin/by/ntnk/msluschedule/db/data/ScheduleContainer.kt
+++ app/src/main/kotlin/by/ntnk/msluschedule/db/data/ScheduleContainer.kt
@@ -2,12 +2,16 @@ package by.ntnk.msluschedule.db.data
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.TypeConverters
+import by.ntnk.msluschedule.db.ScheduleTypeConverter
+import by.ntnk.msluschedule.utils.ScheduleType
 
 @Entity
+@TypeConverters(ScheduleTypeConverter::class)
 data class ScheduleContainer constructor(
         @PrimaryKey val id: Int,
         val name: String,
-        val type: String,
+        val type: ScheduleType,
         val faculty: Int,
         val course: Int,
         val year: Int
---
+++ app/src/main/kotlin/by/ntnk/msluschedule/utils/ScheduleType.kt
@@ -0,0 +1,3 @@
+package by.ntnk.msluschedule.utils
+
+enum class ScheduleType { STUDYGROUP, TEACHER }
