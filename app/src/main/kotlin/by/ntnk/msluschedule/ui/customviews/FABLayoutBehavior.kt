5b9180bb95caaada1375262081b2c93d1b99aaac
Kirill Antonik <antkirp@gmail.com>
2018-05-23 21:12:09 +0300

    ui: main: Hide Floating Action Buttons on scroll

---
+++ app/src/main/kotlin/by/ntnk/msluschedule/ui/customviews/FABLayoutBehavior.kt
@@ -0,0 +1,42 @@
+package by.ntnk.msluschedule.ui.customviews
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import by.ntnk.msluschedule.R
+import android.view.Gravity
+import android.view.animation.AnimationUtils
+
+@Suppress("UNUSED")
+class FABLayoutBehavior(
+        context: Context, attrs: AttributeSet
+) : CoordinatorLayout.Behavior<LinearLayout>(context, attrs) {
+    override fun onStartNestedScroll(
+            coordinatorLayout: CoordinatorLayout, child: LinearLayout,
+            directTargetChild: View, target: View, axes: Int, type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onAttachedToLayoutParams(params: CoordinatorLayout.LayoutParams) {
+        super.onAttachedToLayoutParams(params)
+        params.dodgeInsetEdges = Gravity.BOTTOM
+    }
+
+    override fun onNestedScroll(
+            coordinatorLayout: CoordinatorLayout, child: LinearLayout, target: View,
+            dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int
+    ) {
+        if (child.visibility == View.VISIBLE && dyConsumed > 0) {
+            child.visibility = View.INVISIBLE
+            child.startAnimation(AnimationUtils.loadAnimation(child.context, R.anim.slide_down))
+
+        } else if (child.visibility == View.INVISIBLE && dyConsumed < 0) {
+            child.visibility = View.VISIBLE
+            child.startAnimation(AnimationUtils.loadAnimation(child.context, R.anim.slide_up))
+        }
+    }
+}
