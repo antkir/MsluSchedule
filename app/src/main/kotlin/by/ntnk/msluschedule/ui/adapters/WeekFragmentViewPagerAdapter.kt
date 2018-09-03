d8a07b3ef55f254714180115a2cb753d1238f0d1
Kirill Antonik <antkirp@gmail.com>
2018-09-03 13:46:49 +0300

    ui: WeekFragmentAdapter: Fix incorrect fragment indices

--- app/src/main/kotlin/by/ntnk/msluschedule/ui/adapters/WeekFragmentViewPagerAdapter.kt
+++ app/src/main/kotlin/by/ntnk/msluschedule/ui/adapters/WeekFragmentViewPagerAdapter.kt
@@ -3,7 +3,6 @@ package by.ntnk.msluschedule.ui.adapters
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
-import android.view.ViewGroup
 import by.ntnk.msluschedule.ui.week.WeekFragment
 import by.ntnk.msluschedule.utils.ImmutableEntry
 
@@ -12,22 +11,18 @@ class WeekFragmentViewPagerAdapter(
         private val fragmentsInfo: List<ImmutableEntry>,
         private val currentWeekIndex: Int
 ) : FragmentStatePagerAdapter(fragmentManager) {
-    private val fragments: MutableList<WeekFragment> = ArrayList()
+    private val fragments: MutableList<WeekFragment?> = ArrayList()
 
-    fun getWeekFragment(index: Int): WeekFragment = fragments[index]
+    init {
+        fragmentsInfo.forEach { fragments.add(null) }
+    }
+
+    fun getWeekFragment(index: Int): WeekFragment? = fragments[index]
 
     override fun getItem(position: Int): Fragment {
         val weekId = fragmentsInfo[position].key
-        return WeekFragment.newInstance(weekId, isCurrentWeek = position == currentWeekIndex)
-    }
-
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val fragment: WeekFragment = super.instantiateItem(container, position) as WeekFragment
-        if (fragments.size <= position) {
-            fragments.add(fragment)
-        } else {
-            fragments[position] = fragment
-        }
+        val fragment = WeekFragment.newInstance(weekId, isCurrentWeek = position == currentWeekIndex)
+        fragments[position] = fragment
         return fragment
     }
 
