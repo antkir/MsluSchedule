393ff5b73a090e259c7f794903713c6fba138188
Kiryl Antonik <antkirp@gmail.com>
2020-10-12 03:32:25 +0300

    src: Update code for new Crashlytics

--- app/src/release/kotlin/by/ntnk/msluschedule/AppTimberTree.kt
+++ app/src/release/kotlin/by/ntnk/msluschedule/AppTimberTree.kt
@@ -1,15 +1,16 @@
 package by.ntnk.msluschedule
 
 import android.util.Log
-import com.crashlytics.android.Crashlytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 
 class AppTimberTree : Timber.Tree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        if (priority < Log.WARN) return
-        Crashlytics.log(priority, tag, message)
-        if (t != null) {
-            Crashlytics.logException(t)
+        if (priority >= Log.WARN) {
+            FirebaseCrashlytics.getInstance().log(message)
+            if (t != null) {
+                FirebaseCrashlytics.getInstance().recordException(t)
+            }
         }
     }
 }
